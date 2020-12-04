0080321b8cd4167bde82fa709ef1fbd17f585614
tec <tec@10.10.104.150>
2020-12-04 14:51:01 +0900

    asdfasdf

---
+++ src/main/java/com/han/vo/CarrierVO.java
@@ -0,0 +1,35 @@
+package com.han.vo;
+
+public class CarrierVO {
+	
+	private String carrCode;
+	private String carrName;
+	private String carrRef;
+	private String carrTel;
+	
+	public String getCarrCode() {
+		return carrCode;
+	}
+	public void setCarrCode(String carrCode) {
+		this.carrCode = carrCode;
+	}
+	public String getCarrName() {
+		return carrName;
+	}
+	public void setCarrName(String carrName) {
+		this.carrName = carrName;
+	}
+	public String getCarrRef() {
+		return carrRef;
+	}
+	public void setCarrRef(String carrRef) {
+		this.carrRef = carrRef;
+	}
+	public String getCarrTel() {
+		return carrTel;
+	}
+	public void setCarrTel(String carrTel) {
+		this.carrTel = carrTel;
+	}
+
+}
